1398aac4b483f93614d8c94c9499303c4e8ab4de
g5niusx <g5niusx@gmail.com>
2019-01-14 23:18:08 +0800

    :fire: 删除代码

--- src/main/java/com/java/netty/custom/protocol/CustomProtocol.java
+++ src/main/java/com/java/netty/custom/protocol/CustomProtocol.java
@@ -3,7 +3,6 @@ package com.java.netty.custom.protocol;
 import lombok.Getter;
 
 import java.io.Serializable;
-import java.util.Arrays;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -29,22 +28,5 @@ public class CustomProtocol implements Serializable {
     public CustomProtocol(String message) {
         this.message = message.getBytes(UTF_8);
         this.length = this.message.length;
-    }
-
-    public static void main(String[] args) {
-        CustomProtocol hello_world = new CustomProtocol("hello world");
-        System.out.println(Arrays.toString(hello_world.IntToByte(hello_world.length)));
-        System.out.println(Arrays.toString(hello_world.getMessage()));
-    }
-
-
-    public byte[] IntToByte(int num) {
-        byte[] bytes = new byte[4];
-        bytes[0] = (byte) ((num >> 24) & 0xff);
-        bytes[1] = (byte) ((num >> 16) & 0xff);
-        bytes[2] = (byte) ((num >> 8) & 0xff);
-        bytes[3] = (byte) (num & 0xff);
-        return bytes;
-
     }
 }
